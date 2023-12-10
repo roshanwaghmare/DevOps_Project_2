4c48d8e9d4570402fdf2d7f529a89f403fc7541a
roshanwaghmare <142305817+roshanwaghmare@users.noreply.github.com>
2023-12-11 00:21:06 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "amazon-linux" {
 resource "aws_instance" "dev_machine" {
   ami = data.aws_ami.amazon-linux.id
   instance_type = "t2.micro"
-  key_name = "roshan-jenkins"
+  key_name = "ec2-jenkins"
 
   tags = {
     Environment = "dev"
