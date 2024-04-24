bf2543b892a5dbec5630a53e528f9797cb8a5934
roshanwaghmare <142305817+roshanwaghmare@users.noreply.github.com>
2024-04-24 13:50:44 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,18 +1,20 @@
-
-data "aws_ami" "amazon2" {
+data "aws_ami" "amazon-linux" {
   most_recent = true
 
-  owners = [ amazon ]
   filter {
-    name = "name"
-    values = [ "amzn2-ami-kernel-5.10-hvm-2.0.20240412.0-x86_64-gp2" ]
+    name   = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-2.0.20240412.0-x86_64-gp2"]
+
   }
-  
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
-
 resource "aws_instance" "dev_machine" {
-  ami = data.aws_ami.amazon2.id
+  ami = data.aws_ami.amazon-linux.id
   instance_type = var.instance_type
   key_name = "jenkins"
 
